b7847d477aba733e83031f1178ddb98d3ca0ff9b
Chretien Kalala <chretienkalala22@gmail.com>
2025-11-27 23:18:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import AppBar from './components/header/Appbar.tsx'
+function App() {
+
+  return (
+    <>
+      <div className='max-w-7xl border border-red-500 mx-auto'>
+        <AppBar />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Appbar.tsx
@@ -0,0 +1,9 @@
+export default function AppBar() {
+  return (
+    <header>
+      <nav className=" p-4 text-white">
+        <h1 className="text-2xl text-black font-display font-bold">Jadoo</h1>
+      </nav>
+    </header>
+  );
+}  
